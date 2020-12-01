eb59311eede319515522292bdf3e5d03afc7bdf1
mapi8808 <mapi.008.k@gmail.com>
2020-12-01 09:40:42 +0000

    [add]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,10 +6,21 @@ class BooksController < ApplicationController
   if @book.save
    flash[:notice] = "You have created book successfully."
    redirect_to book_path(@book.id)
-  else
+    else
       @books = Book.all
       render ("books/index")
+    end
   end
+  
+  def update
+  @user = User.find(params[:id])
+     if @user.update(user_params)
+     flash[:update] = "You have updated user successfully."
+     redirect_to user_path(@user)
+     else
+      @user = User.find(params[:id]) 
+      render :edit
+     end
   end
 
   def index
@@ -24,6 +35,15 @@ class BooksController < ApplicationController
   def edit
   @book = Book.find(params[:id]) 
   end
+  
+  def destroy
+   book = Book.find(params[:id])
+   if book.destroy
+   flash[:destroy] = "Book was successfully destroyed."
+   redirect_to books_path
+   end
+  end 
+  
   
   private
   def book_params
