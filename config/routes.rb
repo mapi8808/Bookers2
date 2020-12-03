089ca0aa54eaf35a94326034e21cf2a257b98865
mapi8808 <mapi.008.k@gmail.com>
2020-12-03 13:01:28 +0000

    [add]

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
+  resources :homes
   devise_for :users
   resources :users, only: [:index, :edit, :show, :update]
   resources :books
