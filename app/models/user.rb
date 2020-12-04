c3a10b74c4a2c11d05a6e64cb4629b6ebc88dff7
mapi8808 <mapi.008.k@gmail.com>
2020-12-04 09:04:29 +0000

    [add]

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   attachment :profile_image
   # attachmentはimage? profile_image?
-  validates :name, length: { minimum: 2, maximum: 20 }, presence: true
+  validates :name, length:  { in: 2..20 }, presence: true
   validates :introduction, length: { maximum: 50 }
 end
 
