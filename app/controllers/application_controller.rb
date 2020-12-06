b9550349ba688136fe4b011cabc2b98b5896959e
mapi8808 <mapi.008.k@gmail.com>
2020-12-06 10:03:58 +0000

    [add]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,9 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     user_path(resource)
   end
+  
+  def after_sign_out_path_for(resource)
+    flash[:signout] = "Signed out successfully."
+    root_path
+  end
 end
