98171225e559d311019608e20c2119174fc6bc1e
mapi8808 <mapi.008.k@gmail.com>
2020-12-09 09:45:04 +0000

    [add]

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'homes/about'
+  get 'home/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'homes#top'
-  resources :homes
+  root to: 'home#top'
+  resources :home
   devise_for :users
   resources :users, only: [:index, :edit, :show, :update]
   resources :books
