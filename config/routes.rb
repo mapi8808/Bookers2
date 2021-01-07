c6e988459886408ea9cfe9dc50e06b2d22423591
mapi8808 <mapi.008.k@gmail.com>
2021-01-07 08:52:56 +0000

    [add]

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'book_comments/create'
+  get 'book_comments/destroy'
   get 'home/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#top'
@@ -6,6 +8,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:index, :edit, :show, :update]
   resources :books do
+    resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
  end
 end
