fd76449734a969335c84aee3ba88f4912fc1d9ff
mapi8808 <mapi.008.k@gmail.com>
2021-01-08 06:29:48 +0000

    [update]

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,17 +3,36 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
-  has_many :books, dependent: :destroy
-  has_many :favorites, dependent: :destroy
-  has_many :book_comments, dependent: :destroy
-  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy # フォロー取得
-  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy # フォロワー取得
+  
   attachment :profile_image
   # attachmentはimage? profile_image?
   validates :name, length:  { in: 2..20 }, presence: true, uniqueness: true
   validates :introduction, length: { maximum: 50 }
   validates :email,  presence: true
+         
+  has_many :books, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   
+  has_many :book_comments, dependent: :destroy
+  
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy # フォロー取得
+  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy # フォロワー取得
+  has_many :following_user, through: :follower, source: :followed # 自分がフォローしている人
+  has_many :follower_user, through: :followed, source: :follower # 自分をフォローしている人
+  
+  # ユーザーをフォローする
+  def follow(user_id)
+    follower.create(followed_id: user_id)
+  end
+
+  # ユーザーのフォローを外す
+  def unfollow(user_id)
+    follower.find_by(followed_id: user_id).destroy
+  end
+
+  # フォローしていればtrueを返す
+  def following?(user)
+    following_user.include?(user)
+  end
 end
 
