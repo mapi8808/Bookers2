04fb70a25e6481f6c5cd4c068722b1d8804aa5bf
mapi8808 <mapi.008.k@gmail.com>
2021-01-14 04:50:46 +0000

    [add]

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,17 +1,18 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    unless comment.save
+    @book = Book.find(params[:book_id])
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = @book.id
+    unless @comment.save
     render 'error'
     end
   end
 
   def destroy
     BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
+    @book = Book.find(params[:id])
   end
-  
+
   private
 
   def book_comment_params
